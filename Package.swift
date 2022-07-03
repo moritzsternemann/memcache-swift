c94c490afd659e0403fa23f8eafb490041702265
Moritz Sternemann <opensource@moritzsternemann.de>
2022-07-03 15:51:10 +0200

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "memcache-swift",
+    products: [
+        .library(name: "Memcache", targets: ["Memcache"]),
+        .executable(name: "memcache-swift-example", targets: ["memcache-swift-example"])
+    ],
+    dependencies: [
+         .package(url: "https://github.com/apple/swift-nio.git", from: "2.40.0")
+    ],
+    targets: [
+        .target(
+            name: "Memcache",
+            dependencies: [
+                .product(name: "NIO", package: "swift-nio")
+            ]
+        ),
+        .testTarget(
+            name: "MemcacheTests",
+            dependencies: [
+                .target(name: "Memcache")
+            ]
+        ),
+
+        .executableTarget(
+            name: "memcache-swift-example",
+            dependencies: [
+                .target(name: "Memcache")
+            ]
+        )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,6 @@
+# Memcache Swift
+
+⚠️  work in progress
+
+Non-blocking, event-driven Swift client for [Memcached](http://memcached.org) built on [SwiftNIO](https://github.com/apple/swift-nio).
+

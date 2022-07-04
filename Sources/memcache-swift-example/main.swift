afcb57a50c430b4468ba50713ed999ca5dc5739d
Moritz Sternemann <opensource@moritzsternemann.de>
2022-07-04 17:54:47 +0200

    Short example with set and get

---
+++ Sources/memcache-swift-example/main.swift
@@ -0,0 +1,26 @@
+import Memcache
+import NIO
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: 1)
+defer {
+    try? group.syncShutdownGracefully()
+}
+
+let connection = try MemcacheConnection.make(
+    address: try .init(ipAddress: "127.0.0.1", port: 11211),
+    boundEventLoop: group.next()
+).wait()
+
+print("connected")
+
+let buffer = ByteBuffer(staticString: "Hello World")
+let result = try connection
+    .set("mykey", to: buffer, expiration: .relative(1337))
+    .flatMap { response -> EventLoopFuture<[String]> in
+        print(response)
+        
+        return connection.get("mykey", "mykey2")
+    }
+    .wait()
+
+print("Result: \(String(describing: result))")

b58a436708133ace5e1e097051c6502bf5403931
Moritz Sternemann <opensource@moritzsternemann.de>
2022-07-04 16:09:34 +0200

    Add blank test for now

---
+++ Tests/MemcacheTests/ChannelHandlers/MemcacheCommandHandlerTests.swift
@@ -0,0 +1,13 @@
+//
+//  MemcacheCommandHandlerTests.swift
+//  
+//
+//  Created by Moritz Sternemann on 03.07.22.
+//
+
+import NIO
+import XCTest
+
+@testable import Memcache
+
+final class MemcacheCommandHandlerTests: XCTestCase {}
